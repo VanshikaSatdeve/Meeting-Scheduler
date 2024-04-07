aac784c45dd7004c66c7e83776f5afe3eab57ea2
Vanshi <vsatdeve7@gmail.com>
2024-04-07 13:16:26 +0530

    chnage file

--- src/componants/Logo.js
+++ src/componants/Logo.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import Data from '../Shared/constant'
 
 function Logo() {
     return (
         <div>
             <div className="text-2xl font-bold text-gray-800">Meeting Scheduler</div>
-            {/* <img src={Data.img1} alt='' height="200px" width="200px"/> */}
+            
 
 
         </div>
